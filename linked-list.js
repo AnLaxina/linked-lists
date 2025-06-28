e2236139bd9439e54b3828cd2cde6b79696c0a14
AnLaxina <anilovlaxina@gmail.com>
2025-06-27 22:07:45 -0700

    Add initial files for constructing the Linked List

---
+++ linked-list.js
@@ -0,0 +1,5 @@
+export default class LinkedList {
+    constructor() {
+        console.log("Hello! I'm created");
+    }
+}
---
+++ main.js
@@ -0,0 +1,3 @@
+import LinkedList from "./linked-list.js";
+
+const newLinkedList = new LinkedList();
