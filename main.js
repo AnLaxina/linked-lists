465769b60837a5070c40cbf36826fa13693271d8
AnLaxina <anilovlaxina@gmail.com>
2025-06-27 22:11:05 -0700

    Create & finalize Node class

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
 import LinkedList from "./linked-list.js";
+import Node from "./node.js";
 
-const newLinkedList = new LinkedList();
+const nodey = new Node();
+console.log(nodey.value);
+console.log(nodey.nextNode);
